19078a16cbda2058cccfcda41615266586cd1e27
Himanshu Joshi <himharsh1997@gmail.com>
2019-01-02 16:29:02 +0530

    Date and wind feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,17 @@ import * as $ from 'jquery';
 export class AppComponent {
   title = 'Whether';
 
-  getvalues = ()=>{
+   getvalues = ()=>{
     navigator.geolocation.getCurrentPosition(this.printvalue);
-
   }
   printvalue = (position)=>{
 //document.getElementById('pri').innerHTML = position.coords.latitude + "  " + position.coords.longitude;
-
-
+console.log(position)
 $.ajax({
   type:'GET',
   url: 'https://geocoder.tilehosting.com/r/'+ position.coords.longitude+'/'+position.coords.latitude+'.js?key=jgCKJ4loM3WvBhmSK5zc',
   success: function(data) {
-  //  document.getElementById('pri').innerHTML = JSON.stringify(data);
-    console.log(data);
+document.getElementById('location').innerHTML = data.results[0].display_name
       },
   error: function(jqXHR, textStatus, errorThrown) {
           console.log(jqXHR.status);
@@ -32,21 +29,28 @@ $.ajax({
 
 $.ajax({
   type:'GET',
-  url: 'https://api.openweathermap.org/data/2.5/weather/?lat='+position.coords.latitude +'&lon='+position.coords.longitude+'&APPID=b2b5902633a15cacaa50df2585d7c4f2'
-  ,
+  url : 'https://api.openweathermap.org/data/2.5/weather/?lat='+position.coords.latitude +'&lon='+position.coords.longitude+'&APPID=b2b5902633a15cacaa50df2585d7c4f2',
   success: function(data) {
 
-    document.getElementById('pri').innerHTML = "Your Location: "+ JSON.stringify(data.name) + "    and Wheather:" + (data.weather)[0].description;
-    document.getElementById('imgicon').src = 'http://openweathermap.org/img/w/'+ (data.weather)[0].icon + '.png';
-    document.getElementById('temp').innerHTML = " :  " + (data.main.temp - 274.15).toFixed(2);
-    console.log(data);
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    document.getElementById('imgaicon').src = 'http://openweathermap.org/img/w/'+ (data.weather)[0].icon + '.png';
+    document.getElementById('temp').innerHTML =  (data.main.temp - 274.15).toFixed(0);
+    document.getElementById('w_status').innerHTML = data.weather[0].main;
+    document.getElementById('speed').innerHTML = 'wind:' + data.wind.speed + " Km/hr"
+    var d = new Date();
+    console.log(d.getDate())
+    document.getElementById('date_today').innerHTML = d.getDate() + " " +  months[d.getMonth()] + "," + d.getFullYear();
+
+     console.log(data);
       },
   error: function(jqXHR, textStatus, errorThrown) {
           console.log(jqXHR.status);
       },
- dataType: 'json'
+ dataType: 'jsonp'
 });
 
 }
 
 }
+//  url: 'https://api.openweathermap.org/data/2.5/weather/?lat='+position.coords.latitude +'&lon='+position.coords.longitude+'&APPID=b2b5902633a15cacaa50df2585d7c4f2
+  //  document.getElementById('location').innerHTML = data.name + this.loca.results[0].alternative_names ;

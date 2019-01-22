4ff811d9293c480ab994987ead4f445b0eb1adfb
Himanshu Joshi <himharsh1997@gmail.com>
2019-01-22 21:42:19 +0530

    Project compelete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,20 @@ import * as $ from 'jquery';
 
 export class AppComponent {
   title = 'Whether';
+
 ngAfterViewInit(){
+
   this.getvalues();
 }
    getvalues = ()=>{
+
+     if(new Date().getHours() < 5 || new Date().getHours() > 18)
+     {
+       (document.getElementById('day_night') as HTMLImageElement).src = '../assets/night2.jpeg';
+     }
+     else{
+       (document.getElementById('day_night') as HTMLImageElement).src = '../assets/day.jpeg';
+     }
     navigator.geolocation.getCurrentPosition(this.printvalue);
   }
   printvalue = (position)=>{
@@ -35,7 +45,7 @@ $.ajax({
   success: function(data) {
 
 var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    document.getElementById('imgaicon').src = 'http://openweathermap.org/img/w/'+ (data.weather)[0].icon + '.png';
+    (document.getElementById('imgaicon') as HTMLImageElement).src = 'http://openweathermap.org/img/w/'+ (data.weather)[0].icon + '.png';
     document.getElementById('temp').innerHTML =  (data.main.temp - 274.15).toFixed(0);
     document.getElementById('w_status').innerHTML = data.weather[0].main;
     document.getElementById('speed').innerHTML = 'Wind: ' + data.wind.speed + " km/hr";
@@ -49,7 +59,7 @@ var months = ['January','February','March','April','May','June','July','August',
   error: function(jqXHR, textStatus, errorThrown) {
           console.log(jqXHR.status);
       },
- dataType: 'jsonp'data
+ dataType: 'jsonp'
 });
 
 }
@@ -57,9 +67,10 @@ var months = ['January','February','March','April','May','June','July','August',
 callby_user = ()=>{
   $.ajax({
     type:'GET',
-    url : 'https://api.openweathermap.org/data/2.5/weather/?q=' + (document.getElementById("city_name").value.toString())  +','+  document.getElementById("country_name").value.toString() +'&APPID=b2b5902633a15cacaa50df2585d7c4f2',
+    url : 'https://api.openweathermap.org/data/2.5/weather/?q=' + ((document.getElementById("city_name") as HTMLInputElement).value.toString())  +','+  (document.getElementById("country_name") as HTMLInputElement ).value.toString() +'&APPID=b2b5902633a15cacaa50df2585d7c4f2',
     success: function(data) {
 console.log(data);
+
   document.getElementById('t_text').innerHTML =  "    "  +  (data.main.temp - 274.15).toFixed(0);
   document.getElementById('we_text').innerHTML = "    "  +   data.weather[0].description;
   document.getElementById('wi_text').innerHTML = "    "  +   data.wind.speed + " km/hr"
@@ -67,16 +78,19 @@ console.log(data);
   document.getElementById("result").style.display = "block";
   document.getElementById("result").style.opacity = "0.4";
   document.getElementById("result").style.opacity = "1";
+  document.getElementById('not_found').style.display = 'none';
+
         },
     error: function(jqXHR, textStatus, errorThrown) {
             console.log(jqXHR.status);
+              document.getElementById('not_found').style.display = 'block';
+  document.getElementById("result").style.display = "none";
         },
    dataType: 'jsonp'
   });
 
 
 }
-
 
 }
 //  url: 'https://api.openweathermap.org/data/2.5/weather/?lat='+position.coords.latitude +'&lon='+position.coords.longitude+'&APPID=b2b5902633a15cacaa50df2585d7c4f2
